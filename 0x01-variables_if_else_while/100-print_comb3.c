c23c0b713558fee5d9e45c9bfae7b10462442189
Ojobumiche <bumiche@gmail.com>
2022-12-10 06:48:07 -0800

    ok

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,4 @@
-[06:28, 10/12/2022] Awolabi_alx: #include <stdio.h>
-
-/**
- * Description: main - A program that prints all possible combinations digits
- * You can only use the putchar function
- * Return: 0
- */
-
-int main(void)
-{
-int num1 = 48;
-int num2 = 44;
-while (num1 <= 57)
-{
-putchar(num1);
-if (num1 != 57)
-{
-putchar(num2);
-putchar(32);
-}
-num1 += 1;
-}
-putchar('\n');
-return (0);
-}
-[06:40, 10/12/2022] Awolabi_alx: #include <stdio.h>
-
+#include <stdio.h>
 /**
  * Description: main - program that prints all possible different combs
  * Numbers must be separated by ,followed by a space

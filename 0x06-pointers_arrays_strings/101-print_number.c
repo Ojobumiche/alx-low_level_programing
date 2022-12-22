ecb4d5715c7bc1f6cc01e0f70ee6374fe685acc1
Ojobumiche <bumiche@gmail.com>
2022-12-22 06:17:55 -0800

    go on coding

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,50 @@
 #include "main.h"
 
 /**
- * _strcat - a function that concatenates two strings.
- * @dest: pointer to destination char
- * @src: pointer to source char
+ * print_number - a function that prints and integer.
  * Return: Always 0.
+ * @n: number
  */
+void print_number(int n)
+{
+	unsigned int size = n;
 
-char *_strcat(char *dest, char *src)
-{
-unsigned int x = 0;
-unsigned int y = 0;
-while (*(dest + x) != '\0')
-x++;
-while (*(src + y) != '\0')
-{
-*(dest + x) = *(src + y);
-x++;
-y++;
-}
-return (dest);
+	int num = 0;
+
+	int x;
+
+	if (n < 0)
+		size = -n;
+	if (n == 0)
+		num = 1;
+
+	while (size >= 1)
+	{
+		size = size / 10;
+		num++;
+	}
+
+	for (x = 0; x < num; x++)
+	{
+
+		int pow = 1;
+
+		int y;
+
+		int d;
+
+		for (y = 0; y < num - x - 1; y++)
+		{
+			pow = pow * 10;
+		}
+
+		d = ((n / pow) % 10);
+		if (n < 0)
+		{
+			d = -d;
+			if (x == 0)
+				_putchar(45);
+		}
+		_putchar(48 + d);
+	}
 }

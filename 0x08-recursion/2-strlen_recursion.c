33a46ac68070f2197ab51350b6dbee6bba15c539
Ojobumiche <bumiche@gmail.com>
2023-01-04 12:08:10 -0800

    it is well

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+Task 2: 2-strlen_recursion.c
+
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+
+	}
+	return (i);
+}

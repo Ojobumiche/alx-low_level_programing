45bbb0d21a71ae6ac4bd0568786be7ea1d5a3be9
Ojobumiche <bumiche@gmail.com>
2023-01-04 11:39:29 -0800

    good

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
-/**
- * _print_rev_recursion - print a string in reverse
+/** * _print_rev_recursion - print a string in reverse
  * @s: string
  * Return 0;
 */
@@ -12,4 +11,4 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-}
+}}

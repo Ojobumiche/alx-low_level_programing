fd840554aef77e4c14b75d7cfbd60eb50caaa4e8
Ojobumiche <bumiche@gmail.com>
2023-01-04 20:39:50 -0800

    yes ok

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -38,3 +38,22 @@ int get_pal(char *s, int len)
 	}
 	return (get_pal(s + 1, len - 2));
 }
+
+/**
+ * is_palindrome - a function that returns 1
+ * if a string is a palindrome and 0 if not.
+ * @s: enter char.
+ * Return: Always 0.
+ */
+
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+	if (len <= 1)
+	{
+		return (1);
+	}
+	return (get_pal(s, len));
+}

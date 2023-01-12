5088e6f378f44a41a206d1afa6ffe89fd56c04a1
Ojobumiche <bumiche@gmail.com>
2023-01-12 12:58:11 -0800

    i love holberton school

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _print - moves a string one place to the left and prints the string
@@ -60,7 +61,6 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 	}
 	return (dest);
 }
-
 /**
  * check_for_digits - checks the arguments to ensure they are digits
  * @av: pointer to arguments
@@ -105,7 +105,6 @@ void init(char *str, int l)
  *
  * Return: zero, or exit status of 98 if failure
  */
-
 int main(int argc, char *argv[])
 {
 	int l1, l2, ln, ti, i;
@@ -145,4 +144,4 @@ int main(int argc, char *argv[])
 	}
 	_print(a, ln - 1);
 	return (0);
-}
+}}

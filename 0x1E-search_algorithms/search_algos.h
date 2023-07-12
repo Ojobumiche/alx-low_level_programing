2fe86ae1ef3d140a3ad0633aa034be6f16b26e3d
Ojobumiche <bumiche@gmail.com>
2023-07-12 06:03:00 -0700

    header files updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "search_algos.h"
 /**
  * struct listint_s - singly linked list
  *
@@ -48,5 +48,8 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
 
 #endif
